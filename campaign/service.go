0e6ea15a5a97bd9723eddafac5f28656180e39c1
sands <muhammadarisandi95@gmail.com>
2021-03-13 20:20:33 +0700

    create campaign API

--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,15 @@
 package campaign
 
+import (
+	"time"
+	"github.com/gosimple/slug"
+	"fmt"
+)
+
 type Service interface{
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignById(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct{
@@ -34,5 +41,29 @@ func (s *service) GetCampaignById(input GetCampaignDetailInput) (Campaign, error
 	if err != nil {
 		return campaign, err
 	}
+
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.GoalAmount = input.GoalAmount
+	campaign.Perks = input.Perks
+	campaign.UserID = input.User.ID
+
+	msec := time.Now().UnixNano()
+	slugCandidate := fmt.Sprintf("%s %d %d", input.Name, input.User.ID, msec)
+	campaign.Slug = slug.Make(slugCandidate)
+
+	//pembuatan slug
+	newCampaign, err := s.repository.Save(campaign)	
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
+
 }
--- handler/campaign.go
+++ handler/campaign.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwa_crowdfunding/user"
 	"bwa_crowdfunding/campaign"
 	"bwa_crowdfunding/helper"
 	"net/http"
@@ -49,5 +50,33 @@ func (h *campaignHandler) GetCampaign(c *gin.Context){
 	}
 
 	response := helper.APIResponse("Campaign Detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *campaignHandler) CreateCampaign(c *gin.Context) {
+	var input campaign.CreateCampaignInput 
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors" : errors}
+
+		response := helper.APIResponse("Failed to create campaign", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User) // pake user.User karna balikannya interface kalo dari MustGet
+
+	input.User = currentUser
+
+	newCampaign, err := h.service.CreateCampaign(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	
+	response := helper.APIResponse("Success to create campaign", http.StatusOK, "success", campaign.FormatCampaign(newCampaign))
 	c.JSON(http.StatusOK, response)
 }
